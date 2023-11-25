f53c1569be532f3a5b0acac6e045c293b5f0183e
Brendan <brendan.milton@metrics.com.au>
2023-11-25 15:00:05 +1100

    fix offset warning and wrap HiArrowDown in a div tag to bypass deployment error Type 'Element' is not assignable to type 'ReactNode'.

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -51,7 +51,9 @@ const HeroSection = () => {
           offset={-100}
           duration={500}
         >
+        <div>
           <HiArrowDown size={35} className="animate-bounce" />
+        </div>
         </Link>
       </div>
     </section>
